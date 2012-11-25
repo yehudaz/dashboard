8f1f1e3953c6561d565dc4b6a53fdd4cc6d5a0b7
David Paluy <david@paluy.org>
2012-11-25 14:13:51 +0200

    order fixed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @type = params[:type] || Record.first._type
-    @records = Record.where('_type' => @type).select("created_at, value")
+    @records = Record.where('_type' => @type).order("created_at").select("created_at, value")
   end
 end
