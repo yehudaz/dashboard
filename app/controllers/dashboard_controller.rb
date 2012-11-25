91de788ffb21e65337df68c2952a369c5d15a06c
David Paluy <david@paluy.org>
2012-11-25 16:26:20 +0200

    dashboard fix if no records

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @the_type = params[:the_type] || Record.first.the_type
-    @records = Record.where('the_type' => @type).order("created_at").select("created_at, value")
+    @the_type = (Record. count > 0) ?
+        (params[:the_type] || Record.first.the_type) : "NA"
+    @records = Record.where('the_type' => @the_type).order("created_at").select("created_at, value")
   end
 end
