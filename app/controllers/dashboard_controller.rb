7623e0ea0559ddf329cda09b9fb5a94ed1b07a8a
David Paluy <david@paluy.org>
2012-11-25 21:20:03 +0200

    lower case for PG

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     @the_type = (Record.count > 0) ?
         (params[:the_type] || Record.first.the_type) : "NA"
-    @records = Record.where('the_type LIKE ?', @the_type.downcase).order("created_at").select("created_at, value")
+    @records = Record.where('LOWER(the_type)=?', @the_type.downcase).order("created_at").select("created_at, value")
   end
 end
